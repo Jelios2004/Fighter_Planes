8172332f18e4042624c2b1ac2f23d308b0d7612f
BrodyTownsend <bjtownsend21@gmail.com>
2024-11-17 23:40:56 -0500

    Week 12

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,12 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
     public GameObject player;
     public GameObject enemyOne;
+    public GameObject enemy;
     public GameObject cloud;
     public GameObject powerup;
+    public GameObject LifePowerUp;
     public GameObject sideEnemy;
     public GameObject sideSideEnemy;
 
@@ -21,25 +22,29 @@ public class GameManager : MonoBehaviour
     public int cloudSpeed;
 
     private bool isPlayerAlive;
+    private int score;
+    private int playerLives = 3;  // Player's initial lives
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public TextMeshProUGUI restartText;
     public TextMeshProUGUI powerupText;
-
-    private int score;
+    public TextMeshProUGUI livesText;  // Reference to lives display
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemyOne", 1f, 3f);
+        InvokeRepeating("CreateEnemy", 1f, 3f);
         InvokeRepeating("CreateSideEnemy", 1f, 6f);
         InvokeRepeating("CreateSideSideEnemy", 3f, 3f);
+        InvokeRepeating("CreateLifePowerUp", 3f, 9f);
         StartCoroutine(CreatePowerup());
         CreateSky();
         score = 0;
         scoreText.text = "Score: " + score;
+        livesText.text = "Lives: " + playerLives;  // Initialize lives display
         isPlayerAlive = true;
         cloudSpeed = 1;
     }
@@ -47,12 +52,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Restart();   
+        Restart();
     }
 
     void CreateEnemyOne()
     {
         Instantiate(enemyOne, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.Euler(0, 0, 180));
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
     }
 
     void CreateSideEnemy()
@@ -74,24 +84,42 @@ public class GameManager : MonoBehaviour
         StartCoroutine(CreatePowerup());
     }
 
+    void CreateLifePowerUp()
+    {
+        Instantiate(LifePowerUp, new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), 0), Quaternion.identity);
+    }
+
     void CreateSky()
     {
         for (int i = 0; i < 30; i++)
         {
-            Instantiate(cloud, transform.position, Quaternion.identity);
+            Vector3 cloudPosition = new Vector3(Random.Range(-9f, 9f), Random.Range(5f, 10f), 0);  // Random y position for variety
+            Instantiate(cloud, cloudPosition, Quaternion.identity);
         }
     }
 
     public void EarnScore(int newScore)
     {
-        score = score + newScore;
+        score += newScore;
         scoreText.text = "Score: " + score;
+    }
+
+    public void LoseLife()
+    {
+        playerLives--;  // Decrease lives by 1
+        livesText.text = "Lives: " + playerLives;  // Update the UI text to show remaining lives
+
+        if (playerLives <= 0)
+        {
+            GameOver();
+        }
     }
 
     public void GameOver()
     {
         isPlayerAlive = false;
         CancelInvoke();
+        CancelInvoke("CreateEnemy");  // Stops enemy spawning
         gameOverText.gameObject.SetActive(true);
         restartText.gameObject.SetActive(true);
         cloudSpeed = 0;
@@ -99,7 +127,7 @@ public class GameManager : MonoBehaviour
 
     void Restart()
     {
-        if(Input.GetKeyDown(KeyCode.R) && isPlayerAlive == false)
+        if (Input.GetKeyDown(KeyCode.R) && !isPlayerAlive)
         {
             SceneManager.LoadScene("Game");
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     private float horizontalInput;
     private float verticalInput;
-    private float horizontalScreenSize = 11.5f;
-    private float verticalScreenSize = 7.5f;
     private float speed;
+    private float horizontalScreenLimit = 11.5f;
+    private float verticalScreenLimit = 7.5f;
     private int lives;
     private int shooting;
     private bool hasShield;
@@ -26,9 +24,9 @@ public class Player : MonoBehaviour
     void Start()
     {
         speed = 6f;
-        lives = 3;
-        shooting = 1;
-        hasShield = false;
+        lives = 3; // Initialize player's lives
+        shooting = 1; // Initialize shooting type
+        hasShield = false; // Player starts without a shield
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
@@ -43,12 +41,13 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput,0) * Time.deltaTime * speed);
-        if (transform.position.x > horizontalScreenSize || transform.position.x <= -horizontalScreenSize)
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * speed);
+
+        if (transform.position.x > horizontalScreenLimit || transform.position.x <= -horizontalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
-        if (transform.position.y > 0f || transform.position.y < -verticalScreenSize)
+        if (transform.position.y > verticalScreenLimit || transform.position.y <= -verticalScreenLimit)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
@@ -68,7 +67,7 @@ public class Player : MonoBehaviour
                     Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.identity);
                     break;
                 case 3:
-                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.Euler(0, 0, 30f)); 
+                    Instantiate(bullet, transform.position + new Vector3(-0.5f, 1, 0), Quaternion.Euler(0, 0, 30f));
                     Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
                     Instantiate(bullet, transform.position + new Vector3(0.5f, 1, 0), Quaternion.Euler(0, 0, -30f));
                     break;
@@ -78,20 +77,21 @@ public class Player : MonoBehaviour
 
     public void LoseALife()
     {
-        if (hasShield == false)
+        if (!hasShield)
         {
-            lives--;
+            lives--; // Decrease player's lives
         }
-        else if (hasShield == true)
+        else
         {
-            Debug.Log("true");
-            hasShield = false;
+            hasShield = false; // Disable shield
             shield.gameObject.SetActive(false);
         }
 
+        gameManager.LoseLife(); // Update lives display in GameManager
+
         if (lives == 0)
         {
-            gameManager.GameOver();
+            gameManager.GameOver(); // Trigger game over
             Instantiate(explosion, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
@@ -116,7 +116,6 @@ public class Player : MonoBehaviour
 
     public IEnumerator ShieldPowerDown()
     {
-        Debug.Log("power down");
         yield return new WaitForSeconds(3f);
         gameManager.PlayPowerDown();
         hasShield = false;
@@ -126,35 +125,31 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D whatIHit)
     {
-        if(whatIHit.tag == "Powerup")
+        if (whatIHit.tag == "Powerup")
         {
             gameManager.PlayPowerUp();
-            int powerupType = Random.Range(1, 5); //this can be 1, 2, 3, or 4
-            switch(powerupType)
+            int powerupType = Random.Range(1, 5); // Randomly select a powerup type (1-4)
+            switch (powerupType)
             {
                 case 1:
-                    //speed powerup
                     speed = 9f;
                     gameManager.UpdatePowerupText("Picked up Speed!");
                     thruster.gameObject.SetActive(true);
                     StartCoroutine(SpeedPowerDown());
                     break;
                 case 2:
-                    //double shot
                     shooting = 2;
                     gameManager.UpdatePowerupText("Picked up Double Shot!");
-                    StartCoroutine (ShootingPowerDown());
+                    StartCoroutine(ShootingPowerDown());
                     break;
                 case 3:
-                    //triple shot
                     shooting = 3;
                     gameManager.UpdatePowerupText("Picked up Triple Shot!");
                     StartCoroutine(ShootingPowerDown());
                     break;
                 case 4:
-                    //shield
-                    gameManager.UpdatePowerupText("Picked up Shield!");
                     hasShield = true;
+                    gameManager.UpdatePowerupText("Picked up Shield!");
                     shield.gameObject.SetActive(true);
                     StartCoroutine(ShieldPowerDown());
                     break;
